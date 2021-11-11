441ee35f6d332aa9d83767f6286279215fba6015
Lance Riedel <lanceriedel@gmail.com>
2021-11-10 22:15:40 -0600

    sandbox.py from competition

--- vrc_sandbox/sandbox.py
+++ vrc_sandbox/sandbox.py
@@ -1,9 +1,13 @@
 # This imports the "json" module from the Python standard library
 # https://docs.python.org/3/library/json.html
 import json
+from time import sleep
+from colour import Color
+
+
 
 # This is outside the scope of beginner Python and VRC, but this is for
-# something called "type-hinting" that makes Python code easier to debug
+# something called "type-hin    ting" that makes Python code easier to debug
 from typing import Any, Callable, Dict
 
 # This imports the Paho MQTT library that we will use to communicate to
@@ -25,6 +29,7 @@ class Sandbox():
         # Because we're running this code within a Docker Compose network,
         # using the container name of "mqtt" will work.
         self.mqtt_host = "mqtt"
+        self.isdropping1=0
         # Create an integer attribute to hold the port number of the MQTT server
         # we're going to connect to. MQTT uses a default of port 1883, but we'll
         # add a zero, so as to not require administrator priviledges from the host
@@ -59,6 +64,11 @@ class Sandbox():
             # strings together. Scroll down farther to see what `self.show_velocity` is.
             # https://realpython.com/python-f-strings/#f-strings-a-new-and-improved-way-to-format-strings-in-python
             f"{self.topic_prefix}/velocity": self.show_velocity,
+            f"{self.topic_prefix}/mytestopen": self.open_servo,
+            f"{self.topic_prefix}/mytestclose": self.close_servo,
+            "vrc/apriltags/visible_tags": self.visible_apriltag,
+           
+
         }
 
     # Create a new method to effectively run everything.
@@ -99,11 +109,12 @@ class Sandbox():
     def on_message(self, client: mqtt.Client, userdata: Any, msg: mqtt.MQTTMessage) -> None:
         # Print the topic name and the message payload to the console
         # for debugging purposes.
-        print(f"{msg.topic}: f{str(msg.payload)}")
+        #print(f"{msg.topic}: f{str(msg.payload)}")
 
         # First, check if the topic of the message we've recieved is inside the topic
         # map we've created.
         if msg.topic in self.topic_map:
+            print(f"{msg.topic}: f{str(msg.payload)}")
             # We can't send JSON (dictionary) data over MQTT, so we send it as an
             # encoded string. Here, we convert that encoded string back to
             # JSON information for convience.
@@ -133,7 +144,23 @@ class Sandbox():
     # ================================================================================
     # Here is an example on how to publish a message to an MQTT topic to
     # perform an action
-    def open_servo(self) -> None:
+    def close_servo(self,data: dict) -> None:
+        # First, we construct a dictionary payload per the documentation.
+        data = {"servo": 0, "action": "close"}
+        # This creates it all in one line, however, you could also create it in multiple
+        # lines as shown below.
+        # data = {}
+        # data["servo"] = 0
+        # data["action"] = "open"
+
+        # Now, we convert the dictionary to a JSON encoded string that we can publish.
+        payload = json.dumps(data)
+
+        # Finally, we publish the payload to the topic, once again using f-strings to
+        # re-use our common prefix.
+        self.mqtt_client.publish(topic=f"{self.topic_prefix}/pcc/set_servo_open_close", payload=payload)
+
+    def open_servo(self,data: dict) -> None:
         # First, we construct a dictionary payload per the documentation.
         data = {"servo": 0, "action": "open"}
         # This creates it all in one line, however, you could also create it in multiple
@@ -148,6 +175,123 @@ class Sandbox():
         # Finally, we publish the payload to the topic, once again using f-strings to
         # re-use our common prefix.
         self.mqtt_client.publish(topic=f"{self.topic_prefix}/pcc/set_servo_open_close", payload=payload)
+   
+    def parse_hex_color(self,color):
+        rgb = color.get_rgb()
+        return tuple(round(c*255) for c in rgb)
+
+    def send_color_by_distance_mesg(self,distance):
+
+            print(f"::::::Ready to send color DISTANCE msg: {distance} ") 
+            yellow = Color("yellow")
+            colors = list(yellow.range_to(Color("purple"),30))
+            sendcolor = [255, 255, 255, 255]
+            if distance > 29:
+                distance = 29
+            colorid = round(distance)
+
+            hexcolorval = colors[colorid]
+            rgbtuple = self.parse_hex_color(hexcolorval)
+            sendcolor[1] = rgbtuple[0]
+            sendcolor[2] = rgbtuple[1]
+            sendcolor[3] = rgbtuple[2]
+
+            msg = {
+                        "wrgb": sendcolor
+                        }
+            debugmsg = json.dumps(msg)
+            print(f":Sending color DISTANCE msg: {debugmsg} ") 
+            self.mqtt_client.publish(f"vrc/pcc/set_base_color", json.dumps(msg)) 
+
+    def send_color_mesg(self, color):
+
+            print(f"::::::Ready to send color msg: {color} ") 
+
+            blue = [255, 0, 0, 255]
+            red = [255, 255, 0, 0]
+            green = [255, 0, 255, 0]
+            yellow = [255,255,255,0]
+            orange = [255,255,40,0]
+            purple = [255,138,0,139]
+
+
+            if color=='orange':
+                msg = {
+                        "wrgb": orange
+                        }
+            elif color=='red':
+                msg = {
+                        "wrgb": red
+                        }
+            elif color=='blue':
+                msg = {
+                        "wrgb": blue
+                        }
+            elif color=='green':
+                msg = {
+                        "wrgb": green
+                        }
+            elif color=='purple':
+                msg = {
+                        "wrgb": purple
+                        }
+            else: 
+                msg = {
+                        "wrgb": yellow
+                        }   
+
+            debugmsg = json.dumps(msg)
+            print(f":Sending color msg: {debugmsg} ") 
+            self.mqtt_client.publish(f"vrc/pcc/set_base_color", json.dumps(msg))  
+
+    def visible_apriltag(self,data: dict) -> None:
+            #NOTE: this allows you to map tag id servo number -- (first slot is bogus because of 0 count e.g. tagid 1 = servo 0, tagid 2 = servo 1)  
+            tagidmap=[0, 0,1,3]
+
+
+            tag = data[0]
+            tagid = tag["id"]
+
+            #Change colors for pathways
+            if (tagid==5):
+                self.send_color_mesg("red") 
+            elif (tagid==6):
+                self.send_color_mesg("green")
+            elif (tagid==7):
+                self.send_color_mesg("blue") 
+
+            #Handle building/package drops  
+            else:
+                horizontal_dist = tag["horizontal_dist"] 
+                vertical_dist = tag["vertical_dist"] 
+                heading = tag["heading"] 
+                print(f"heading: {heading} ")
+                print(f"vertical_dist: {vertical_dist} ")
+                print(f"****** XXhorizontal_dist: {horizontal_dist} ")
+                
+                
+                if (horizontal_dist<10):
+
+                    print(f"******------------- ABOUT TO DROP, TURNING GREEN!!!!!!!!!!!!!!****************")
+                    self.send_color_mesg("green") 
+    
+                    print(f"******!!!!!!!!!!!! SERVO DROP:: tagid:{tagid} servo num: {tagidmap[tagid]}!!!!!!!!!!!!!!****************")
+
+                    datamsg = {"servo": tagidmap[tagid], "action": "open"}
+                    payload = json.dumps(datamsg)
+                    self.mqtt_client.publish(topic=f"{self.topic_prefix}/pcc/set_servo_open_close", payload=payload)
+                    
+
+
+                elif (horizontal_dist>10):
+                    print(f"******------COLOR GRADIENT!!!!!!!!!!!!!****************")
+                    self.send_color_by_distance_mesg(horizontal_dist)  
+                
+            # elif (horizontal_dist>30):
+                #    self.send_color_mesg("orange")  
+
+                
+        
 
 if __name__ == "__main__":
     box = Sandbox()
